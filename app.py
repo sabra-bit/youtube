3818fd3c9b926ba7ed1814476f971b132b9ff084
sabra-bit <71123248+sabra-bit@users.noreply.github.com>
2023-09-07 18:03:33 +0300

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from pytube import YouTube ,Playlist
 import requests ,openpyxl
+from youtube_transcript_api import YouTubeTranscriptApi
 
 import pandas as pd
 import nltk
